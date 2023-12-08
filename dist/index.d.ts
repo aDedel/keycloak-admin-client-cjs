e284d04138792aa3fa049768f39e82b0c87695c9
Alexandre DEL VECCHIO <alexdelvecchio@gmail.com>
2023-12-08 15:50:21 +0100

    fix(interfaces): add missing types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -63,10 +63,13 @@ import SynchronizationResultRepresentation from '@keycloak/keycloak-admin-client
 import SystemInfoRepresentation from '@keycloak/keycloak-admin-client/lib/defs/systemInfoRepersantation'
 import TestLdapConnectionRepresentation from '@keycloak/keycloak-admin-client/lib/defs/testLdapConnection'
 import UserConsentRepresentation from '@keycloak/keycloak-admin-client/lib/defs/userConsentRepresentation'
-import UserProfileConfig from '@keycloak/keycloak-admin-client/lib/defs/userProfileConfig'
+import UserProfileConfig from '@keycloak/keycloak-admin-client/lib/defs/userProfileMetadata'
 import UserSessionRepresentation from '@keycloak/keycloak-admin-client/lib/defs/userSessionRepresentation'
 import WhoAmIRepresentation from '@keycloak/keycloak-admin-client/lib/defs/whoAmIRepresentation'
 import { Credentials, Settings, TokenResponse, TokenResponseRaw } from '@keycloak/keycloak-admin-client/lib/utils/auth'
+import { ConnectionConfig } from '@keycloak/keycloak-admin-client/lib/client'
+import { UserQuery } from '@keycloak/keycloak-admin-client/lib/resources/users'
+import { RequestArgs } from '@keycloak/keycloak-admin-client/lib/resources/agent'
 
 export { KeycloakAdminClient }
 export type {
@@ -141,4 +144,7 @@ export type {
   Settings,
   TokenResponse,
   TokenResponseRaw,
+  ConnectionConfig,
+  UserQuery,
+  RequestArgs,
 }
